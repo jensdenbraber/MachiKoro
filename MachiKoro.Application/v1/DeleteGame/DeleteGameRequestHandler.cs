c32ffaa690b5dcbf8fa14caad69f0b7f6f4f2f06
Jens den Braber <jens.denbraber@gmail.com>
2021-07-28 22:09:52 +0200

    Delete a game via API

---
+++ MachiKoro.Application/v1/DeleteGame/DeleteGameRequestHandler.cs
@@ -0,0 +1,34 @@
+﻿using MachiKoro.Application.v1.Interfaces;
+using MachiKoro.Core.Models.DeleteGame;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MachiKoro.Application.v1.CreateGame
+{
+    public class DeleteGameRequestHandler : IRequestHandler<DeleteGameRequest, DeleteGameResponse>
+    {
+        private readonly IGamesRepository _gameRepository;
+
+        public DeleteGameRequestHandler(IGamesRepository gameRepository)
+        {
+            _gameRepository = gameRepository ?? throw new ArgumentNullException(nameof(gameRepository));
+        }
+
+        public async Task<DeleteGameResponse> Handle(DeleteGameRequest request, CancellationToken cancellationToken)
+        {
+            request = request ?? throw new ArgumentNullException(nameof(request));
+
+            bool created = await _gameRepository.DeleteAsync(request.GameId);
+
+            if (!created)
+                return null;
+
+            return new DeleteGameResponse()
+            {
+                GameId = request.GameId
+            };
+        }
+    }
+}
---
+++ MachiKoro.Core/Models/DeleteGame/DeleteGameResponse.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MachiKoro.Core.Models.DeleteGame
+{
+    public class DeleteGameResponse
+    {
+        public Guid? GameId { get; set; }
+    }
+}
