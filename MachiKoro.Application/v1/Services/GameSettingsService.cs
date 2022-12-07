1ddddedc50166e8c9fe830a6a498bedea86517b1
Jens den Braber <jens.denbraber@gmail.com>
2022-12-07 17:34:23 +0530

    work in progress

---
+++ MachiKoro.Application/v1/Services/GameSettingsService.cs
@@ -0,0 +1,15 @@
+﻿using MachiKoro.Domain.Enums;
+using MachiKoro.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MachiKoro.Application.v1.Services;
+
+public class GameSettingsService : IGameSettings
+{
+
+}
---
+++ MachiKoro.Domain/Interfaces/IGameSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MachiKoro.Domain.Interfaces;
+
+public interface IGameSettings
+{
+}
